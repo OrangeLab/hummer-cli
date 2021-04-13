484b9613fda85f8bb6d51ab6e92ac66ca3385c48
duanlikang <duanlikang@didichuxing.com>
2021-04-13 21:39:30 +0800

    feat: add tenon style loader

--- packages/plugin-build/src/config/tenon.config.ts
+++ packages/plugin-build/src/config/tenon.config.ts
@@ -1,10 +1,13 @@
 import { Configuration } from 'webpack'
+import {TenonStylePlugin} from '@hummer/tenon-style-loader'
 import {VueLoaderPlugin} from '@hummer/tenon-loader'
 import JsccPlugin from 'webpack-plugin-jscc'
 import {ProjectConfig} from '@hummer/cli-utils'
 
+import * as path from 'path'
+
 export default function getDefaultTenonConfiguration(isProduction: boolean, hmConfig?:ProjectConfig): Configuration {
-  let plugins = [new VueLoaderPlugin()]
+  let plugins:any = []
   if(hmConfig){
     // TODO 自定义插件的配置，在这里进行拓展
     // TODO Validate Jscc Config
@@ -75,8 +78,13 @@ export default function getDefaultTenonConfiguration(isProduction: boolean, hmCo
             ]
           }
         }
-      }]
+      }, {
+        test: /\.css$/,
+        use: {
+          loader: path.join(require.resolve('@hummer/tenon-style-loader'))
+        }
+      },]
     },
-    plugins: plugins
+    plugins: [new TenonStylePlugin(),new VueLoaderPlugin(), ...plugins]
   }
 }
