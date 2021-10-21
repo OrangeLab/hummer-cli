bb4e0f94c8b4d85cd736248f87ea54491d09ef2c
guangyuan <hishiguangyuan@didiglobal.com>
2021-10-21 19:29:15 +0800

    feat: 添加 获取所有 dev 页面接口

--- packages/plugin-debug/src/proxy/entry.ts
+++ packages/plugin-debug/src/proxy/entry.ts
@@ -2,8 +2,9 @@ import * as http from 'http'
 import { InspectorProxy } from './InspectorProxy'
 const {parse} = require('url');
 
-export default function runInspectorProxy(port:number, projectRoot:string) {
+export default function runInspectorProxy(port:number, devPort:number, projectRoot:string) {
   const inspectorProxy = new InspectorProxy(projectRoot);
+  inspectorProxy.devPort = devPort;
   const httpServer = http.createServer(inspectorProxy.processRequest.bind(inspectorProxy))
   httpServer.listen(port, 'localhost', () => {
     const websocketEndpoints = inspectorProxy.addWebSocketListener(
